c7c35723347d08091a3528e45f0f36454b1053f2
Renata Vieira <rrenattavs@gmail.com>
2023-06-07 11:13:00 -0600

    change app file

--- src/App.jsx
+++ src/App.jsx
@@ -16,12 +16,14 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Renata Vieira</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        <p>git remote add origin git@github.com:rrenattavs/portfolio.git
+git branch -M main
+git push -u origin main
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
