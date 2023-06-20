5f82a16d15e2f45d7412b5cd02eab5c2849e572b
Renata Vieira <rrenattavs@gmail.com>
2023-06-20 12:21:32 -0600

    Added navibar and some styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import './App.css'
+import NavigationBar from './screens/navigationBar/Navigationbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <NavigationBar />
+      
       </div>
-      <h1>Renata Vieira</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>git remote add origin git@github.com:rrenattavs/portfolio.git
-git branch -M main
-git push -u origin main
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/helpers/NavigationLinks.jsx
@@ -0,0 +1,38 @@
+export const NavigationLinks =[
+  {
+    name: 'Home',
+    ref: '#home'
+  },
+  {
+    name: 'About me',
+    ref: '#about-me'
+  },
+  {
+    name: 'Experience',
+    ref: '#experience'
+  },
+  {
+    name: 'Technologies',
+    ref: '#technologies'
+  },
+  {
+    name: 'Portfolio',
+    ref: '#portfolio'
+  },
+  {
+    name: 'Contact',
+    ref: '#contact'
+  }
+]
+
+// const NavigationLink = () => {
+//   {
+//     name: 'HOME',
+//     ref: '#HOME'
+//   }
+//   return (
+//     <div>NavigationLink</div>
+//   )
+// }
+
+// export default NavigationLink
