375bd57ac799c1735610b6f2ca0d1673d014d702
Renata Vieira <rrenattavs@gmail.com>
2023-11-10 17:09:08 -0700

    Changes on the styles

--- src/screens/header/Header.jsx
+++ src/screens/header/Header.jsx
@@ -1,21 +1,19 @@
-import Typed from 'react-typed'
+import Typed from "react-typed";
 // import Type from 'react-type'
 
 const Header = () => {
   return (
     <div>
-        <h1 className='page-wrapper'>Who am I?</h1>
-        <Typed className='page-wrapper'
-            strings={[
-                'Full Stack Developer',
-                'Renata Vieira'
-            ]}
-            typeSpeed={49}
-            backSpeed={50}
-            loop
-        />
+      <h1 className="page-wrapper">Who am I?</h1>
+      <Typed
+        className="page-wrapper"
+        strings={["Full Stack Developer", "Renata Vieira"]}
+        typeSpeed={49}
+        backSpeed={50}
+        loop
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/screens/navigationBar/NavigationBar.jsx
+++ src/screens/navigationBar/NavigationBar.jsx
@@ -1,32 +1,32 @@
-import "./NavigationBar.css"
-import "../../App.css"
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import { NavigationLinks } from '../../helpers/NavigationLinks'
+// import "./NavigationBar.css"
+// import "../../App.css"
+// import Navbar from 'react-bootstrap/Navbar'
+// import Nav from 'react-bootstrap/Nav'
+// import { NavigationLinks } from '../../helpers/NavigationLinks'
 
 
-const NavigationBar = () => {
+// const NavigationBar = () => {
     
-    const createLink=()=>{
-        return NavigationLinks.map((e, idx) => (
-            <Nav.Link key={idx} href ={e.ref}>{e.name}</Nav.Link>
-        ))
-    }
+//     const createLink=()=>{
+//         return NavigationLinks.map((e, idx) => (
+//             <Nav.Link key={idx} href ={e.ref}>{e.name}</Nav.Link>
+//         ))
+//     }
 
-  return (
-    <div id="home">
-        <Navbar className="navigation__container" expand="md">
-            <Navbar.Brand style={{marginLeft: '1rem'}} className = "navbar-brand" href="#home">RVIEIRAS</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse style = {{justifyContent: 'flex-end', marginRight: '1rem', borderColor: 'none'}}>
-                <Nav className='links' style = {{margin: '0 1rem'}}>
-                {createLink()}
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-    </div>
-  )
+//   return (
+//     <div id="home">
+//         <Navbar className="navigation__container" expand="md">
+//             <Navbar.Brand style={{marginLeft: '1rem'}} className = "navbar-brand" href="#home">RVIEIRAS</Navbar.Brand>
+//             <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//             <Navbar.Collapse style = {{justifyContent: 'flex-end', marginRight: '1rem', borderColor: 'none'}}>
+//                 <Nav className='links' style = {{margin: '0 1rem'}}>
+//                 {createLink()}
+//                 </Nav>
+//             </Navbar.Collapse>
+//         </Navbar>
+//     </div>
+//   )
 
-}
+// }
 
-export default NavigationBar
+// export default NavigationBar
