7bfb26fa78aaa1f2eed8365786051ca47154c8db
Renata Vieira <rrenattavs@gmail.com>
2023-12-07 14:23:18 -0700

    save point

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,25 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 // import './App.css'
-import NavigationBar from './screens/navigationBar/NavigationBar.jsx';
+// import NavigationBar from "./screens/navigationBar/NavigationBar.jsx";
 // import Particles from './components/config/Particles.jsx';
-import Background from './components/config/Background.jsx';
-import Header from './screens/header/header.jsx';
-// 
+// import Background from "./components/config/Background.jsx";
+// import Header from "./screens/header/header.jsx";
+//
 
 function App() {
-
   return (
     <>
-      <div className='App'>
-          {/* <div className="app-nav"> */}
-            <NavigationBar />
-            <Header />
-            {/* </div> */}
-            {/* <Particles /> */}
-            <Background />
-          
-
+      <div className="App">
+        {/* <div className="app-nav"> */}
+        {/* <NavigationBar /> */}
+        {/* <Header /> */}
+        {/* </div> */}
+        {/* <Particles /> */}
+        {/* <Background /> */}
+        <h2>Hello</h2>
       </div>
     </>
-  )
+  );
 }
 
 export default App;
